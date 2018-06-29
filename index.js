7f90e7f1a84da9103fc8bd33263e046a11f964d5
Adam Chambers <adammchambers@gmail.com>
2018-06-28 20:26:02 -0400

    Set up button logic

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ const ui = {
   initBtns: document.querySelector('[data-init]')
 }
 
-console.log(ui)
+ui.initBtns.addEventListener('click', e => init(e, ui))
 
-ui.initBtns.addEventListener('click', e => init(ui))
-
-function init(ui) {
-  console.log(ui)
+function init(e, ui) {
+  const target = e.target
+  const type = target.getAttribute('data-init')
   const loading = `
     Calculating mad science...
   `
   ui.canvas.innerHTML = loading
+
+  if (type === 'url') {
+    
+  } else if (type === 'lucky') {
+    fire()
+  }
+}
+
+function fire() {
+  alert('Fire!!')
 }
